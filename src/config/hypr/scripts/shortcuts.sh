189f55e1b056b1cbd620bf88544e667cf4adb5b6
William C. Canin <william.costa.canin@gmail.com>
2026-06-11 14:46:50 -0300

    chore: commit temp 8

--- src/config/hypr/scripts/shortcuts.sh
+++ src/config/hypr/scripts/shortcuts.sh
@@ -4,9 +4,9 @@ ROFI="/usr/bin/rofi"
 
 case "${LC_MESSAGES:-${LANG:-en}}" in
 pt*)
-  cat "$HOME/.config/hypr/docs/shortcuts/pt.txt" | $ROFI -dmenu -p "Procurar" -i -theme-str 'window { height: 600px;}'
+  cat "$HOME/.config/hypr/docs/shortcuts/pt.txt" | $ROFI -dmenu -p "Procurar" -i -theme-str 'window { width: 1050px; height: 600px;}'
   ;;
 *)
-  cat "$HOME/.config/hypr/docs/shortcuts/en.txt" | $ROFI -dmenu -p "Search" -i -theme-str 'window { height: 600px;}'
+  cat "$HOME/.config/hypr/docs/shortcuts/en.txt" | $ROFI -dmenu -p "Search" -i -theme-str 'window { width: 1050px; height: 600px;}'
   ;;
 esac
